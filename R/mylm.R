521f7cafdad90b311bbab70ab749eccf410a9b00
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-09-11 13:10:49 +0200

    mylm: Add mylm module template

---
+++ R/mylm.R
@@ -0,0 +1,81 @@
+
+# Select Build, Build and reload to build and lode into the R-session.
+
+mylm <- function(formula, data = list(), contrasts = NULL, ...){
+  # Extract model matrix & responses
+  mf <- model.frame(formula = formula, data = data)
+  X  <- model.matrix(attr(mf, "terms"), data = mf, contrasts.arg = contrasts)
+  y  <- model.response(mf)
+  terms <- attr(mf, "terms")
+
+
+  # Add code here to calculate coefficients, residuals, fitted values, etc...
+  # and store the results in the list est
+  # beta <- solve(t(X) %*% X) %*% X %*% y
+  est <- list(terms = terms, model = mf)
+
+  # Store call and formula used
+  est$call <- match.call()
+  est$formula <- formula
+
+  # Set class name. This is very important!
+  class(est) <- 'mylm'
+
+  # Return the object with all results
+  return(est)
+}
+
+print.mylm <- function(object, ...){
+  # Code here is used when print(object) is used on objects of class "mylm"
+  # Useful functions include cat, print.default and format
+  cat('Info about object\n')
+}
+
+summary.mylm <- function(object, ...){
+  # Code here is used when summary(object) is used on objects of class "mylm"
+  # Useful functions include cat, print.default and format
+  cat('Summary of object\n')
+}
+
+plot.mylm <- function(object, ...){
+  # Code here is used when plot(object) is used on objects of class "mylm"
+
+}
+
+
+
+# This part is optional! You do not have to implement anova
+anova.mylm <- function(object, ...){
+  # Code here is used when anova(object) is used on objects of class "mylm"
+
+  # Components to test
+  comp <- attr(object$terms, "term.labels")
+
+  # Name of response
+  response <- deparse(object$terms[[2]])
+
+  # Fit the sequence of models
+  txtFormula <- paste(response, "~", sep = "")
+  model <- list()
+  for(numComp in 1:length(comp)){
+    if(numComp == 1){
+      txtFormula <- paste(txtFormula, comp[numComp])
+    }
+    else{
+      txtFormula <- paste(txtFormula, comp[numComp], sep = "+")
+    }
+    formula <- formula(txtFormula)
+    model[[numComp]] <- lm(formula = formula, data = object$model)
+  }
+
+  # Print Analysis of Variance Table
+  cat('Analysis of Variance Table\n')
+  cat(c('Response: ', response, '\n'), sep = '')
+  cat('          Df  Sum sq X2 value Pr(>X2)\n')
+  for(numComp in 1:length(comp)){
+    # Add code to print the line for each model tested
+  }
+
+  return(model)
+
+}
